358772c20625afd04112c877a48bef2acc596547
totram dang <ttdanglf@gmail.com>
2020-01-05 12:36:49 -0800

    Calculator Requirement 2
    Remove the maximum constraint for numbers e.g. 1,2,3,4,5,6,7,8,9,10,11,12 will return 78

--- RCalculatorTests/RCalculatorTest.cs
+++ RCalculatorTests/RCalculatorTest.cs
@@ -37,5 +37,11 @@ namespace RCalculatorTests
         {
             rc.Input_Num("4,-3");
         }
+
+        [TestMethod]
+        public void No_Max_Num()
+        {
+            rc.Input_Num("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15");
+        }
     }
 }
